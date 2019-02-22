d44c6a73f41ad36e664f7562154879955ad714f4
anjalipatil656 <32560600+anjalipatil656@users.noreply.github.com>
2019-02-22 11:44:05 +0530

    Update Even_odd.c

--- Even_odd.c
+++ Even_odd.c
@@ -4,9 +4,6 @@ int main()
     int result;
     int a[] = "asdfghjk";
     b =b ;
-    int * pInt = NULL; 
-    int p = n/0;
-    free();
     printf("Enter an integer: ");
     scanf("%d", &number);
     if(number % 2 == 0)
@@ -16,10 +13,4 @@ int main()
 
     return 0;
 }
-void calc()
-{
-    int d,c,add;
-    add = c+d;
-    
-    
-}
+
